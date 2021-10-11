81912113cc36d592975a42751e2a49f0de5bb17a
Artiom12341 <70652258+Artiom12341@users.noreply.github.com>
2021-10-11 10:18:27 +0300

    Update Sir de caractere 11.10.2021.py

--- Sir de caractere 11.10.2021.py
+++ Sir de caractere 11.10.2021.py
@@ -1,3 +1,4 @@
 nume=str(input("Dati numele persoanei-"))
 profesie=str(input("Dati profesia acestia-"))
-print ('Numele meu este {0} si doresc sa divin {1}'.format(nume ,profesie))
+print ('''Numele meu este {0} 
+       Si doresc sa divin {1}'''.format(nume ,profesie))
